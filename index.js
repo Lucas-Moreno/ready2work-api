bc153fa2af5be0036196e9364c677217723a459f
Lucas Moreno <moreno.lucas.82300@hotmail.fr>
2021-07-13 16:00:47 +0200

    init package

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello")
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+require("dotenv").config()
+const process = require("process")
+
+const app = express()
+
+var corsOptions = {
+  origin: "http://localhost:80",
+}
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+require("./src/routes/routes.js")(app)
+
+// set port, listen for requestsxs
+const PORT = process.env.PORT || 3000
+app.listen(PORT, err => {
+    if(err) throw err;
+  console.log(`Server is running on port ${PORT}.`)
+})
+
+module.exports = app
