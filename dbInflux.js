4ae8c3369b02e997628b7cbc221b6c1f5a98f1f7
Lucas Moreno <moreno.lucas.82300@hotmail.fr>
2021-07-21 16:37:04 +0200

    add api/room/:id by data influx

--- dbInflux.js
+++ dbInflux.js
@@ -13,151 +13,67 @@ const getAllRoom = async (req, res) => {
   const queryApi = client.getQueryApi(org)
 
   const query = `
-        from(bucket: "Ready2work")
-            |> range(start: -3h)
-            |> filter(fn: (r) => r["_measurement"] == "mqtt_consumer")
-            |> filter(fn: (r) => r["_field"] == "data_value")
-            |> group(columns: ["topic"])
-            |> yield(name: "mean")
-    `
+  from(bucket: "Ready2work")
+    |> range(start: -3h)
+    |> filter(fn: (r) => r["_measurement"] == "Temperature" or r["_measurement"] == "NbPers" or r["_measurement"] == "Luminosité" or r["_measurement"] == "Bruit")
+    |> filter(fn: (r) => r["_field"] == "data_value")
+    |> filter(fn: (r) => r["nodeID"] == "${req.params.id}")
+    |> yield(name: "mean")
+  `
 
+  let mesureTable = []
   let obj = {}
+  let noise = {}
+  let temperature = {}
+  let brightness = {}
+  let nbPers = {}
 
   try {
     queryApi.queryRows(query, {
       next(row, tableMeta) {
         const o = tableMeta.toObject(row)
-        let room = req.params.id
-
-        switch (true) {
-          case o.topic.includes("A101") && room === "A101":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
+        mesureTable.push(o)
+        for (const mesure of mesureTable) {
+          console.log(mesure._measurement)
+          if (mesure._measurement === "Bruit") {
+            noise = {
+              measurement: mesure._measurement,
+              value: mesure._value,
             }
-            break
-          case o.topic.includes("A102") && room === "A102":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
+          }
+          if (mesure._measurement === "Temperature") {
+            temperature = {
+              measurement: mesure._measurement,
+              value: mesure._value,
             }
-            break
-          case o.topic.includes("A103") && room === "A103":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
+          }
+          if (mesure._measurement === "Luminosité") {
+            brightness = {
+              measurement: mesure._measurement,
+              value: mesure._value,
             }
-            break
-          case o.topic.includes("A104") && room === "A104":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
+          }
+          if (mesure._measurement === "NbPers") {
+            nbPers = {
+              measurement: mesure._measurement,
+              value: mesure._value,
             }
-            break
-          case o.topic.includes("A105") && room === "A105":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("A106") && room === "A106":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("A107") && room === "A107":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("A108") && room === "A108":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("A109") && room === "A109":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("A110") && room === "A110":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("B101") && room === "B101":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("B102") && room === "B102":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("B103") && room === "B103":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("B104") && room === "B104":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("B105") && room === "B105":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("B106") && room === "B106":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
-          case o.topic.includes("B107") && room === "B107":
-            obj = {
-              room: req.params.id,
-              measurement: o._measurement,
-              temperature: o._value,
-            }
-            break
+          }
+        }
+        obj = {
+          room: req.params.id,
+          noise,
+          temperature,
+          brightness,
+          nbPers
         }
       },
       error(error) {
         console.error(error)
       },
       complete() {
-        const objectLength = Object.keys(obj).length
-        console.log(objectLength)
-        if (objectLength > 0) {
+        var hello = Object.keys(obj).length
+        if (hello > 0) {
           return response201WithData(res, obj)
         } else {
           return response400WithMessage(res, "this room doesn't exist")
